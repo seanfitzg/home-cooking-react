d95e638bb677eb230b21b8fc522ad40a948ade5f
Seán Fitzgerald <sean.fitzgerald@hmhco.com>
2020-09-16 12:45:26 +0100

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -2,25 +2,29 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import AddEditRecipe from './Components/RecipeList/AddEditRecipe';
 import RecipeList from './Components/RecipeList/RecipeList';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Routes = () => {
+  const { isAuthenticated } = useAuth0();
   return (
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/recipes">
+    isAuthenticated && (
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/recipes">
+            <RecipeList />
+          </Redirect>
+        </Route>
+        <Route exact path="/recipes">
           <RecipeList />
-        </Redirect>
-      </Route>
-      <Route exact path="/recipes">
-        <RecipeList />
-      </Route>
-      <Route path="/add">
-        <AddEditRecipe />
-      </Route>
-      <Route path="/edit">
-        <AddEditRecipe />
-      </Route>
-    </Switch>
+        </Route>
+        <Route path="/add">
+          <AddEditRecipe />
+        </Route>
+        <Route path="/edit">
+          <AddEditRecipe />
+        </Route>
+      </Switch>
+    )
   );
 };
 
