720832bc5f610c82caa228e41bef13388062aec9
Seán Fitzgerald <seanfitzg@gmail.com>
2021-10-28 16:51:04 +0100

    small nginx change

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   development: {
-    recipes_api: 'http://localhost:5000/recipes',
+    recipes_api: 'http://localhost:5001/recipes',
   },
   production: {
     recipes_api: 'https://homecooking.azurewebsites.net/recipes',
