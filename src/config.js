6d7f72ad9b936e5c3406aa79e28e683315cd1e05
Seán Fitzgerald <seanfitzg@gmail.com>
2021-11-04 17:26:07 +0000

    Added dapr config var

--- src/config.js
+++ src/config.js
@@ -1,12 +1,28 @@
 const api = {
   development: {
-    recipesApi: 'http://localhost:5001/recipes',
+    recipesApi: 'http://localhost:5000/recipes',
+  },
+  dapr: {
+    recipesApi:
+      'http://localhost:3500/v1.0/invoke/home-cooking-app/method/recipes',
   },
   production: {
     recipesApi: 'https://home-cooking-fastify.herokuapp.com/recipes',
   },
 };
-export const apiUri = api[process.env.REACT_APP_ENV].recipesApi;
+
+const getApi = () => {
+  if (
+    process.env.REACT_APP_USE_DAPR &&
+    process.env.REACT_APP_USE_DAPR.toUpperCase() === 'TRUE'
+  ) {
+    return api.dapr.recipesApi;
+  } else {
+    return api[process.env.REACT_APP_ENV].recipesApi;
+  }
+};
+
+export const apiUri = getApi();
 
 export const options = {
   audience: 'https://home-cooking/api',
